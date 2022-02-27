6c42dd2a923bbfb273acbe863cc003fd017386b7
Leo Holman <lah2719@g.rit.edu>
2022-02-26 20:29:14 -0500

    :sparkles: initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <h1>lahdev</h1>
+  </header>
+  <main>
+    <p>Hi, I'm Leo.</p>
+    <p>
+      I'm a full stack web dev. If you've found this page, it likely means you
+      already know who I am and what I do.
+    </p>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
+<style>
+</style>
